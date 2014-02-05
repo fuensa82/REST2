6507280da2931a6525c78c1af110684b71c2736a
fuensa82 <fuensa82@gmail.com>
2014-02-05 23:26:01 +0100

    Primera subida

---
+++ server.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var mysql = require('mysql');
+ 
+var client = mysql.createConnection({
+  user: 'nodejs1',
+  password: '03885536',
+  host: '127.0.0.1',
+  port: '3306',
+  database: 'nodejs1'
+});
+var app = express();
+
+
+app.use(express.bodyParser());
+
+
+app.get('/pilotos',function(req, res){
+  console.log("PILOTOS");
+  client.connect(function(err) {
+    if ( !err ) {
+      console.log("Connected to MySQL");
+    } else if ( err ) {
+      console.log(err);
+    }
+  });
+   
+  client.query(
+      'SELECT * FROM usuarios',
+      function selectUsuario(err, results, fields) {
+   
+        if (err) {
+            console.log("Error: " + err.message);
+          
+        }else{     
+          client.end();
+          res.json(results);
+        }
+      }
+  );
+});
+app.get('/piloto/:id',function(req, res){
+  console.log("PILOTO "+req.params.id);
+  client.connect(function(err) {
+    if ( !err ) {
+      console.log("Connected to MySQL");
+    } else if ( err ) {
+      console.log(err);
+    }
+  });
+  console.log("query");
+  client.query(
+      'SELECT * FROM usuarios where id=?', [req.params.id],
+      function selectUsuario(err, results, fields) {
+        if (err) {
+          console.log("Error: " + err.message);
+        }else{
+          // client.end();
+          res.json(results);
+        }
+
+      }
+  );
+});
+
+app.listen(3434);
